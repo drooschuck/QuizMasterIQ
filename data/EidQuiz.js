6e07c4c7effa7e4f28c4ac3451ada2aebddafb48
drooschuck <drooschuck@gmail.com>
2025-04-01 05:56:29 +0100

    update data folder

---
+++ data/EidQuiz.js
@@ -0,0 +1,112 @@
+const quizArr = [
+  {
+    id: 1,
+    question: "What is the significance of Eid al-Fitr?",
+    options: {
+      A: "Celebration of the end of Ramadan",
+      B: "Commemoration of the Prophet's birthday",
+      C: "Celebration of the Hajj pilgrimage",
+      D: "Marking the beginning of Ramadan",
+    },
+    answer: "A",
+  },
+  {
+    id: 2,
+    question: "What is the traditional dish often eaten during Eid al-Fitr?",
+    options: {
+      A: "Biryani",
+      B: "Samosa",
+      C: "Kheer",
+      D: "Pasta",
+    },
+    answer: "C",
+  },
+  {
+    id: 3,
+    question: "What is the name of the prayer performed on Eid?",
+    options: {
+      A: "Dhuhr",
+      B: "Fajr",
+      C: "Eid Salah",
+      D: "Asr",
+    },
+    answer: "C",
+  },
+  {
+    id: 4,
+    question: "What is given to the poor before the Eid al-Fitr prayer?",
+    options: {
+      A: "Zakat al-Fitr",
+      B: "Sadaqah",
+      C: "Khums",
+      D: "Fidyah",
+    },
+    answer: "A",
+  },
+  {
+    id: 5,
+    question: "Which month does Eid al-Fitr fall in?",
+    options: {
+      A: "Shawwal",
+      B: "Ramadan",
+      C: "Dhu al-Hijjah",
+      D: "Muharram",
+    },
+    answer: "A",
+  },
+  {
+    id: 6,
+    question: "What is a common greeting exchanged during Eid?",
+    options: {
+      A: "Merry Christmas",
+      B: "Happy New Year",
+      C: "Eid Mubarak",
+      D: "Happy Diwali",
+    },
+    answer: "C",
+  },
+  {
+    id: 7,
+    question: "What is the name of the festival that celebrates the willingness of Ibrahim to sacrifice his son?",
+    options: {
+      A: "Eid al-Fitr",
+      B: "Eid al-Adha",
+      C: "Mawlid",
+      D: "Laylat al-Miraj",
+    },
+    answer: "B",
+  },
+  {
+    id: 8,
+    question: "What is the customary practice after the Eid prayer?",
+    options: {
+      A: "Fasting",
+      B: "Feasting",
+      C: "Mourning",
+      D: "Traveling",
+    },
+    answer: "B",
+  },
+  {
+    id: 9,
+    question: "Which animal is commonly sacrificed during Eid al-Adha?",
+    options: {
+      A: "Cow",
+      B: "Goat",
+      C: "Sheep",
+      D: "All of the above",
+    },
+    answer: "D",
+  },
+  {
+    id: 10,
+    question: "What is the purpose of wearing new clothes on Eid?",
+    options: {
+      A: "To show off wealth",
+      B: "To celebrate the occasion",
+      C: "To impress others",
+      D: "To follow tradition",
+    },
+    answer: "B",
+  },
+];
